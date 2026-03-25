24d551fb94ecb980a4ced138f98fcf9d78257a64
Thiago <waquimthiago7@gmail.com>
2026-03-25 12:37:19 -0300

    ligando banco

--- config/database.php
+++ config/database.php
@@ -5,15 +5,15 @@ class Database {
     public static function getConnection() {
         if (self::$pdo === null) {
 
-            $host = "mysql.railway.internal";
-            $db = "railway";
-            $user = "root";
-            $pass = "KNvgfiCrUkjdCSjRnIbdOvjuQdeIVrPp";
-            $port = "3306";
+            $host = "dpg-d71vurh5pdvs7390p230-a.oregon-postgres.render.com";
+            $db = "clinica_4hbx";
+            $user = "clinica_user";
+            $pass = "COLOCA_TUA_SENHA_AQUI";
+            $port = "5432";
 
             try {
                 self::$pdo = new PDO(
-                    "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+                    "pgsql:host=$host;port=$port;dbname=$db",
                     $user,
                     $pass,
                     [
@@ -22,7 +22,37 @@ class Database {
                     ]
                 );
             } catch (PDOException $e) {
-                die("Erro: " . $e->getMessage());
+                die("Erro na conexão: " . $e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
+}
+?><?php
+class Database {
+    private static $pdo = null;
+
+    public static function getConnection() {
+        if (self::$pdo === null) {
+
+            $host = "dpg-d71vurh5pdvs7390p230-a.oregon-postgres.render.com";
+            $db = "clinica_4hbx";
+            $user = "clinica_user";
+            $pass = "EpoECRClHLPv2eQjXuhIMFt6xx0hNrt6";
+            $port = "5432";
+
+            try {
+                self::$pdo = new PDO(
+                    "pgsql:host=$host;port=$port;dbname=$db",
+                    $user,
+                    $pass,
+                    [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    ]
+                );
+            } catch (PDOException $e) {
+                die("Erro na conexão: " . $e->getMessage());
             }
         }
         return self::$pdo;
