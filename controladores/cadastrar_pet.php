cd51c05575a8b2a30c32abf121e4502a366c6f97
Thiago <waquimthiago7@gmail.com>
2026-03-30 13:33:04 -0300

    fix require pet lowercase definitivo

--- controladores/cadastrar_pet.php
+++ controladores/cadastrar_pet.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require_once __DIR__ . "/../config/database.php";
-require_once __DIR__ . "/../classes/Pet.php";
+require_once __DIR__ . "/../classes/pet.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $pdo = Database::getConnection();
